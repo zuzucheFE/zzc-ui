14309a94e65a593dd646b4e40dd9dec982524443
samciu <samciu@samciudeMacBook-Pro.local>
2017-03-22 11:10:11 +0800

    优化Section demo

--- test/Section/src/App.js
+++ test/Section/src/App.js
@@ -5,26 +5,25 @@ import React, {Component} from "react";
 import './style.scss';
 import {Section} from "../../../index"
 
-export default class App extends Component{
+export default class App extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-        }
+        this.state = {}
     }
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Section>
-                    <Section.Header
-                        title='title'
-                        extra='副标题'
-                        onClick={() => {alert('123')}}
-                    ></Section.Header>
+                    <Section.Header title='title' extra='副标题' onClick={() => {
+                        alert('123')
+                    }}/>
                     <Section.Body>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim harum illum itaque odit totam vitae voluptates. Asperiores cumque explicabo ipsam ipsum iste iure iusto, maxime quasi repudiandae similique sunt tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim harum illum itaque odit totam
+                            vitae voluptates. Asperiores cumque explicabo ipsam ipsum iste iure iusto, maxime quasi
+                            repudiandae similique sunt tempore.</p>
                     </Section.Body>
                 </Section>
             </div>
