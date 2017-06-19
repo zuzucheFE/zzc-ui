41cd1d502206967b2370288fd61e283e1d360e9e
linhao <linhao.4zhong@163.com>
2017-06-19 10:45:05 +0800

    fix:修改Modal的字符渲染和jsx渲

--- components/Modal/index.jsx
+++ components/Modal/index.jsx
@@ -2,19 +2,19 @@
  * Created by samciu on 16/12/23.
  */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './index.scss'
 
 class Modal extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor( props ) {
+        super( props );
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
 
-        if(this.props.visible){
+        if ( this.props.visible ) {
             this.refs.modalBox.style.display = 'block';
             setTimeout(() => {
                 this.refs.modalMask.className = 'modal-mask show';
@@ -22,10 +22,10 @@ class Modal extends Component {
                     this.refs.modalWrap.className = 'modal-wrap show';
                     setTimeout(() => {
                         this.refs.modalContent.className = 'modal-content-box show';
-                    },350);
-                },100);
-            },14);
-        }else{
+                    }, 350 );
+                }, 100 );
+            }, 14 );
+        } else {
             this.refs.modalBox.className = 'hide-modal';
             this.refs.modalWrap.className = 'modal-wrap';
             setTimeout(() => {
@@ -33,8 +33,8 @@ class Modal extends Component {
                 setTimeout(() => {
                     this.refs.modalContent.className = 'modal-content-box';
                     this.refs.modalBox.style.display = 'none';
-                },100);
-            },300);
+                }, 100 );
+            }, 300 );
         }
 
     }
@@ -42,17 +42,26 @@ class Modal extends Component {
     render() {
 
         return (
-            <div ref="modalBox" style={{display:'none'}}>
+            <div ref="modalBox" style={{ display: 'none' }}>
                 <div ref="modalMask" className="modal-mask"></div>
                 <div ref="modalWrap" className="modal-wrap">
-                    <div className="modal-back" onClick={this.props.onCancel}/>
+                    <div className="modal-back" onClick={this.props.onCancel} />
                     <div className="modal-content">
-                        <div
-                            className="modal-content-box"
-                            ref="modalContent"
-                            dangerouslySetInnerHTML={{__html: this.props.content}}
-                        >
-                        </div>
+                        {
+                            typeof this.props.content == 'string' ?
+                                <div
+                                    className="modal-content-box"
+                                    ref="modalContent"
+                                    dangerouslySetInnerHTML={{ __html: this.props.content }}
+                                >
+                                </div> :
+                                <div
+                                    className="modal-content-box"
+                                    ref="modalContent"
+                                >
+                                    {this.props.content}    
+                                </div>
+                        }
                     </div>
                     <div className="modal-close" onClick={this.props.onCancel}></div>
                 </div>
